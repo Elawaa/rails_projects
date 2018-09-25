facdac5b50026198ec11345e2aa895bb9eac5c86
Elawaa <mahmoud.elawaa@yahoo.com>
2018-09-25 18:59:29 -0400

    show the errors and the data by the id from the web page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,47 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
+  end
+  
+  
+  
+  
+  def show
+    @article = Article.find(params[ :id])
+  end 
+  
+  
+  
+  def index
+    @article = Article.all
   
   end
+  
+  
+  
+  
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notic] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+  
+  
+  
+  
+  
+  
   
   private
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+
 
 
 
